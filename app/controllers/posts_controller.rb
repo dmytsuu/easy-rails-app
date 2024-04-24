af75ce4382eee22fedee4e7e5a2e636460e20831
dmytsuu <grayeyed16@gmail.com>
2024-04-24 05:26:55 +0300

    update rubocop.yml

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,14 +24,10 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user = current_user
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to post_url(@post), notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to post_url(@post), notice: 'Post was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
