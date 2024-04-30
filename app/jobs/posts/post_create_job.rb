733e2d2c9cdf361604f55857796d3da603609a2e
dmytsuu <grayeyed16@gmail.com>
2024-04-30 19:12:16 +0300

    rubocop -A

--- app/jobs/posts/post_create_job.rb
+++ app/jobs/posts/post_create_job.rb
@@ -1,7 +1,11 @@
-class Posts::PostCreateJob < ApplicationJob
-  queue_as :default
+# frozen_string_literal: true
 
-  def perform(id)
-    Post.find(id).update(title: 'update from the background job')
+module Posts
+  class PostCreateJob < ApplicationJob
+    queue_as :default
+
+    def perform(id)
+      Post.find(id).update(title: 'update from the background job')
+    end
   end
 end
